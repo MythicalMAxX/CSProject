ac29e786c2c1a72753b97a99071b45615716aec8
MythicalMAxX <73683711+MythicalMAxX@users.noreply.github.com>
2021-04-05 00:31:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import discord
 import smtplib
+from discord.utils import get
 from discord.ext import commands
 import mysql.connector
 
 
-mydb = mysql.connector.connect(host="localhost", user="root", password="************", database="mydb")
+mydb = mysql.connector.connect(host="localhost", user="root", password="14147714", database="vinamradb")
 
 mycursor = mydb.cursor()
-
-def schedule(data):
-    fil = open("schedule.txt", "a")
-    fil.write(data)
-    fil.close
-    return data
 
 
 def mailer(data, reciever, subject):
     message = data
-    sender = "***************.com"
-    password = "****"
+    sender = "pokemongo1003player@gmail.com"
+    password = "14147714"
     body = 'Subject: {}\n\n{}'.format(subject, message)
     with smtplib.SMTP("smtp.gmail.com", 587) as smtpserver:
         smtpserver.ehlo()
@@ -29,21 +24,39 @@ def mailer(data, reciever, subject):
 
 
 
-TOKEN = "*******************************************"
-client = commands.Bot(command_prefix="")
+TOKEN = "ODE2Njg5MTQ1NjczNTQ3Nzg2.YD-nCQ.vaACh6IGeVwulyjJQ06ic-gdTLc"
+client = commands.Bot(command_prefix="!")
+
 
 @client.event
 async def on_ready():
     print("bot is ready to takeoff")
 
+@client.command()
+async def make_channel(ctx):
+    guild = ctx.guild
+    member = ctx.author
+    admin_role = get(guild.roles, name="Admin")
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        member: discord.PermissionOverwrite(read_messages=True),
+        admin_role: discord.PermissionOverwrite(read_messages=True)
+        }
+    channel = await guild.create_text_channel('Private', overwrites=overwrites)
+    await channel.send("Welcome")
 
-
-@client.command(aliases=['add_entry', 'Add_entry', 'add', 'Add'])
-@commands.has_permissions(kick_members=True)
-async def ADD(ctx, *, data):
-    string = schedule(data)
-    await ctx.send(string)
-
+@client.command()
+async def make_new(ctx):
+    guild = ctx.guild
+    member = ctx.author
+    admin_role = get(guild.roles, name="Admin")
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=True),
+        member: discord.PermissionOverwrite(read_messages=True),
+        admin_role: discord.PermissionOverwrite(read_messages=True)
+        }
+    wachannel = await guild.create_text_channel('Public', overwrites=overwrites)
+    await wachannel.send("Welcome")
 
 @client.command(aliases=['Send_mail', 'SEND_MAIL'])
 @commands.has_permissions(kick_members=True)
@@ -56,46 +69,46 @@ async def send_mail(ctx, data, reciever, subject):
 
 
 @client.command(aliases=['marks5'])
-async def Marks5(ctx, *, data):
+async def Marks5(ctx,subject:str,chapter:str,topic:str, *, data):
     await ctx.send("pending")
-    sql = "INSERT INTO Question5Marks (questions) VALUES (%s)"
-    val = [data]
+    sql = "INSERT INTO Question5Marks (subject,chapter,topic,questions) VALUES (%s,%s,%s,%s)"
+    val = [subject,chapter,topic,data]
     mycursor.execute(sql, val)
     mydb.commit()
     await ctx.send("Question Added")
 
 @client.command(aliases=['marks4'])
-async def Marks4(ctx, *, data):
+async def Marks4(ctx,subject:str,chapter:str,topic:str, *, data):
     await ctx.send("pending")
-    sql = "INSERT INTO Question4Marks (questions) VALUES (%s)"
-    val = [data]
+    sql = "INSERT INTO Question4Marks (subject,chapter,topic,questions) VALUES (%s,%s,%s,%s)"
+    val = [subject,chapter,topic,data]
     mycursor.execute(sql, val)
     mydb.commit()
     await ctx.send("Question Added")
 
 @client.command(aliases=['marks3'])
-async def Marks3(ctx, *, data):
+async def Marks3(ctx,subject:str,chapter:str,topic:str, *, data):
     await ctx.send("pending")
-    sql = "INSERT INTO Question3Marks (questions) VALUES (%s)"
-    val = [data]
+    sql = "INSERT INTO Question3Marks (subject,chapter,topic,questions) VALUES (%s,%s,%s,%s)"
+    val = [subject,chapter,topic,data]
     mycursor.execute(sql, val)
     mydb.commit()
     await ctx.send("Question Added")
 
 @client.command(aliases=['marks2'])
-async def Marks2(ctx, *, data):
+async def Marks2(ctx,subject:str,chapter:str,topic:str, *, data):
     await ctx.send("pending")
-    sql = "INSERT INTO Question2Marks (questions) VALUES (%s)"
-    val = [data]
+    sql = "INSERT INTO Question2Marks (subject,chapter,topic,questions) VALUES (%s,%s,%s,%s)"
+    val = [subject,chapter,topic,data]
     mycursor.execute(sql, val)
     mydb.commit()
-    await ctx.send("Question Added")@client.command(aliases=['marks5'])
+    await ctx.send("Question Added")
 
 @client.command(aliases=['marks1'])
-async def Marks1(ctx, *, data):
+async def Marks1(ctx,subject:str,chapter:str,topic:str, *, data):
     await ctx.send("pending")
-    sql = "INSERT INTO Question1Marks (questions) VALUES (%s)"
-    val = [data]
+    sql = "INSERT INTO Question1Marks (subject,chapter,topic,questions) VALUES (%s,%s,%s,%s)"
+    val = [subject,chapter,topic,data]
     mycursor.execute(sql, val)
     mydb.commit()
     await ctx.send("Question Added")
@@ -105,35 +118,35 @@ async def Marks1(ctx, *, data):
 @client.command(aliases=['Show_mark5', 'Show_Mark5', 'SHOW_MARK5'])
 @commands.has_permissions(kick_members=True)
 async def show_mark5(ctx):
-    mycursor.execute("SELECT * FROM Question5Marks ORDER BY RAND() LIMIT 1;")
+    mycursor.execute("SELECT questions FROM Question5Marks ORDER BY RAND() LIMIT 1;")
     myresult = mycursor.fetchone()
     await ctx.send(myresult)
 
 @client.command(aliases=['Show_mark4', 'Show_Mark4', 'SHOW_MARK4'])
 @commands.has_permissions(kick_members=True)
 async def show_mark4(ctx):
-    mycursor.execute("SELECT * FROM Question4Marks ORDER BY RAND() LIMIT 1;")
+    mycursor.execute("SELECT questions FROM Question4Marks ORDER BY RAND() LIMIT 1;")
     myresult = mycursor.fetchone()
     await ctx.send(myresult)
 
 @client.command(aliases=['Show_mark3', 'Show_Mark3', 'SHOW_MARK3'])
 @commands.has_permissions(kick_members=True)
 async def show_mark3(ctx):
-    mycursor.execute("SELECT * FROM Question3Marks ORDER BY RAND() LIMIT 1;")
+    mycursor.execute("SELECT questions FROM Question3Marks ORDER BY RAND() LIMIT 1;")
     myresult = mycursor.fetchone()
     await ctx.send(myresult)
 
 @client.command(aliases=['Show_mark2', 'Show_Mark2', 'SHOW_MARK2'])
 @commands.has_permissions(kick_members=True)
 async def show_mark2(ctx):
-    mycursor.execute("SELECT * FROM Question2Marks ORDER BY RAND() LIMIT 1;")
+    mycursor.execute("SELECT questions FROM Question2Marks ORDER BY RAND() LIMIT 1;")
     myresult = mycursor.fetchone()
     await ctx.send(myresult)
 
 @client.command(aliases=['Show_mark1', 'Show_Mark1', 'SHOW_MARK1'])
 @commands.has_permissions(kick_members=True)
 async def show_mark1(ctx):
-    mycursor.execute("SELECT * FROM Question1Marks ORDER BY RAND() LIMIT 1;")
+    mycursor.execute("SELECT questions FROM Question1Marks ORDER BY RAND() LIMIT 1;")
     myresult = mycursor.fetchone()
     await ctx.send(myresult)
 
@@ -141,67 +154,157 @@ async def show_mark1(ctx):
 @client.command(aliases=['Showall_mark5', 'Showall_Mark5', 'SHOWALL_MARK5'])
 @commands.has_permissions(kick_members=True)
 async def showall_mark5(ctx):
-    mycursor.execute("SELECT * FROM Question5Marks;")
+    mycursor.execute("SELECT questions FROM Question5Marks;")
     myresult = mycursor.fetchall()
-    await ctx.send(myresult)
+    for x in myresult:
+        await ctx.send(myresult)
+
 
 @client.command(aliases=['Showall_mark4', 'Showall_Mark4', 'SHOWALL_MARK4'])
 @commands.has_permissions(kick_members=True)
 async def showall_mark4(ctx):
-    mycursor.execute("SELECT * FROM Question4Marks;")
+    mycursor.execute("SELECT questions FROM Question4Marks;")
     myresult = mycursor.fetchall()
     await ctx.send(myresult)
 
 @client.command(aliases=['Showall_mark3', 'Showall_Mark3', 'SHOWALL_MARK3'])
 @commands.has_permissions(kick_members=True)
 async def showall_mark3(ctx):
-    mycursor.execute("SELECT * FROM Question3Marks;")
+    mycursor.execute("SELECT questions FROM Question3Marks;")
     myresult = mycursor.fetchall()
     await ctx.send(myresult)
 
 @client.command(aliases=['Showall_mark2', 'Showall_Mark2', 'SHOWALL_MARK2'])
 @commands.has_permissions(kick_members=True)
 async def showall_mark2(ctx):
-    mycursor.execute("SELECT * FROM Question2Marks;")
+    mycursor.execute("SELECT questions FROM Question2Marks;")
     myresult = mycursor.fetchall()
     await ctx.send(myresult)
 
 @client.command(aliases=['Showall_mark1', 'Showall_Mark1', 'SHOWALL_MARK1'])
 @commands.has_permissions(kick_members=True)
 async def showall_mark1(ctx):
-    mycursor.execute("SELECT * FROM Question1Marks;")
+    mycursor.execute("SELECT questions FROM Question1Marks;")
     myresult = mycursor.fetchall()
     await ctx.send(myresult)
 
-
-
-
-
-@client.command(aliases=['Kick'])
+@client.command(aliases=['Filtersubject'])
 @commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason="You have been kicked for bot testing"):
-    try:
-        await member.send("you have been kicked for testing purpose try rejoining with this link: https://discord.gg/w6Atau")
-    except:
-        await ctx.send("dm is closed")
-    await member.kick(reason=reason)
+async def filtersubject(ctx,*,data):
+    sql = "SELECT questions FROM Question5Marks WHERE Subject = (%s);"
+    val = [data]
+    mycursor.execute(sql,val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question4Marks WHERE Subject = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question3Marks WHERE Subject = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question2Marks WHERE Subject = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question1Marks WHERE Subject = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
 
+@client.command(aliases=['Filtertopic'])
+@commands.has_permissions(kick_members=True)
+async def filtertopic(ctx,*,data):
+    sql = "SELECT questions FROM Question5Marks WHERE Topic = (%s);"
+    val = [data]
+    mycursor.execute(sql,val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question4Marks WHERE Topic = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question3Marks WHERE Topic = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question2Marks WHERE Topic = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question1Marks WHERE Topic = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+
+@client.command(aliases=['FilterChapter'])
+@commands.has_permissions(kick_members=True)
+async def filterChapter(ctx,*,data):
+    sql = "SELECT questions FROM Question5Marks WHERE Chapter = (%s);"
+    val = [data]
+    mycursor.execute(sql,val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question4Marks WHERE Chapter = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question3Marks WHERE Chapter = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question2Marks WHERE Chapter = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+    sql = "SELECT questions FROM Question1Marks WHERE Chapter = (%s);"
+    val = [data]
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        await ctx.send(myresult)
+
+
+@client.command()
+@commands.has_role("Admin")
+async def removechannel(ctx, channel: discord.TextChannel):
+    await channel.delete()
+    await ctx.send("```Successfully deleted the channel!```")
 
 @client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
+async def on_command_error(ctx,error):
+    if isinstance(error,commands.MissingPermissions):
         await ctx.send("Kripya Aukaat Ansusaar Commands Ka Prayog Kare")
-
-    async def on_command_error(ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Incomplete Command")
-            await ctx.message.deleat()
-
-
-@client.command(aliases=['d'])
-@commands.has_permissions(manage_guild=True)
-async def deleat(ctx, amount=2):
-    await ctx.channel.purge(limit=amount)
-
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Incomplete Command")
+        await ctx.message.deleat()
 
 client.run(TOKEN)
